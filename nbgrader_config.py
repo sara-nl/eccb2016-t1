20854a8ce927c2ce977e9634f049c513244c805b
vmk <vm.katttenberg@gmail.com>
2016-08-31 10:21:38 +0200

    Initial commit; nbgrader config

---
+++ nbgrader_config.py
@@ -0,0 +1,12 @@
+c = get_config()
+
+###############################################################################
+# Begin additions by nbgrader quickstart
+###############################################################################
+
+# You only need this if you are running nbgrader on a shared
+# server set up.
+c.NbGrader.course_id = "eccb2016-t1"
+
+# Update this list with other assignments you want
+c.NbGrader.db_assignments = [dict(name="mr"), dict(name="spark")]
