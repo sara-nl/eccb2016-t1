77fd18ae6103a8f84a116f9640c021e1c669ca3e
vmk <vm.katttenberg@gmail.com>
2016-09-02 15:51:34 +0200

    Nbgrader config and "autograded" cells

--- notebooks/eccb2016-t1/nbgrader_config.py
+++ notebooks/eccb2016-t1/nbgrader_config.py
@@ -9,4 +9,12 @@ c = get_config()
 c.NbGrader.course_id = "eccb2016-t1"
 
 # Update this list with other assignments you want
-c.NbGrader.db_assignments = [dict(name="mr"), dict(name="spark")]
+c.NbGrader.db_assignments = [dict(name="Set 1 - Tutorial"), dict(name="Set 2 - Extra")]
+
+c.NbGrader.db_students = [
+    dict(id="bitdiddle", first_name="Ben", last_name="Bitdiddle"),
+    dict(id="hacker", first_name="Alyssa", last_name="Hacker"),
+    dict(id="reasoner", first_name="Louis", last_name="Reasoner")
+]
+
+c.ClearSolutions.code_stub = '# PLEASE FILL IN YOUR CODE HERE'
